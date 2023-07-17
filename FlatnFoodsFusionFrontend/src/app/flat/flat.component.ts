8658bab6cbc5c0ad6a8c0ecc315348e0cdd5016a
Akarsh <abhagat@argusoft.com>
2023-07-17 17:20:45 +0530

    Flat Component

---
+++ FlatnFoodsFusionFrontend/src/app/flat/flat.component.ts
@@ -0,0 +1,32 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-flat',
+  templateUrl: './flat.component.html',
+  styleUrls: ['./flat.component.css']
+})
+export class FlatComponent implements OnInit,
+  AfterViewInit {
+    @ViewChild('tempButton') buttontemp: any;
+    title = 'FlatnFoodsFusion';
+  
+    flatForm: FormGroup | undefined;
+  constructor(private fb: FormBuilder) {
+  }
+  
+  ngOnInit(): void {
+    this.flatForm= this.fb.group({
+      FlatDescription: this.fb.control('dfd'),
+      Price: this.fb.control(''),
+      Address: this.fb.control(''),
+      Contact: this.fb.control(''),
+      Type: this.fb.control(''),
+    });
+  }
+  
+    ngAfterViewInit(): void {
+      this.buttontemp.nativeElement.click();
+    }  
+  }
